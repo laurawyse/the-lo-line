7708e53e72f1bc73fa3a478e8a4f14f908073b85
wyselm <laura@cloud-elements.com>
2014-11-05 21:19:02 -0700

    "adding more photos and light box gallery"

--- public/js/the-lo-line.js
+++ public/js/the-lo-line.js
@@ -1,3 +1,12 @@
-$('.img-portfolio').click(function () {
-
+$('.gallery').magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    // other options
+    gallery: {enabled: true},
+    tLoading : 'Loading image #%curr%...',
+    mainClass : 'mfp-img-mobile',
+    tError : '<a href="%url%">The image #%curr%</a> could not be loaded.',
+    titleSrc: function (item) {
+        return item.el.attr('title');
+    }
 });
